963f6685c3d7f85935506d65352e5354b3adb93e
cosylanguages <cosylanguages@gmail.com>
2025-06-29 12:50:19 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import { PlanProvider } from './PlanContext';
+import { AuthProvider } from './AuthContext';
+import { I18nProvider } from './i18n/I18nContext';
+import { LatinizationProvider } from './contexts/LatinizationContext';
+import { ProgressProvider } from './contexts/ProgressContext';
+import { UserProfileProvider } from './contexts/UserProfileContext'; // Added UserProfileProvider
+import './index.css'; 
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter basename="/cosylanguagesproject">
+        <I18nProvider>
+          <LatinizationProvider>
+            <AuthProvider>
+              <UserProfileProvider> {/* Added UserProfileProvider */}
+                <ProgressProvider> 
+                  <PlanProvider>
+                    <AppRoutes /> 
+                  </PlanProvider>
+                </ProgressProvider>
+              </UserProfileProvider>
+            </AuthProvider>
+          </LatinizationProvider>
+        </I18nProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Failed to find the root element. Please ensure your HTML file has a <div id='root'></div>.");
+}
