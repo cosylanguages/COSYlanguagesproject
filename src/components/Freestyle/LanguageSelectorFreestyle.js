1a0d2bd76e696cc1d604673e51f168ad97364e7a
cosylanguages <cosylanguages@gmail.com>
2025-06-30 17:00:35 +0000

    111

--- src/components/Freestyle/LanguageSelectorFreestyle.js
+++ src/components/Freestyle/LanguageSelectorFreestyle.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useI18n } from '../../i18n/I18nContext'; 
 import '../LanguageSelector/LanguageSelector.css'; // Import unified CSS
 
-const LanguageSelectorFreestyle = ({ selectedLanguage, onLanguageChange }) => {
-    const { allTranslations } = useI18n();
+const LanguageSelectorFreestyle = ({ selectedLanguage }) => {
+    const { allTranslations, changeLanguage, currentLangKey } = useI18n();
 
     const availableLanguages = Object.keys(allTranslations).map(langKey => {
         let name = allTranslations[langKey]?.languageNameNative || 
@@ -20,9 +20,8 @@ const LanguageSelectorFreestyle = ({ selectedLanguage, onLanguageChange }) => {
     });
 
     const handleChange = (event) => {
-        if (onLanguageChange) {
-            onLanguageChange(event.target.value);
-        }
+        const newLangKey = event.target.value;
+        changeLanguage(newLangKey); // Change la langue globale du site
     };
 
     useEffect(() => {
@@ -30,12 +29,12 @@ const LanguageSelectorFreestyle = ({ selectedLanguage, onLanguageChange }) => {
         // Supprimer les anciennes classes de fond
         const classesToRemove = Array.from(body.classList).filter(cls => cls.endsWith('-bg') || cls === 'lang-bg');
         classesToRemove.forEach(cls => body.classList.remove(cls));
-        if (selectedLanguage) {
-            const langClassName = `${selectedLanguage}-bg`;
+        if (currentLangKey) {
+            const langClassName = `${currentLangKey}-bg`;
             body.classList.add(langClassName);
             body.classList.add('lang-bg');
         }
-    }, [selectedLanguage]);
+    }, [currentLangKey]);
 
     return (
         <div className="language-selector-container"> {/* Use unified class */}
@@ -44,7 +43,7 @@ const LanguageSelectorFreestyle = ({ selectedLanguage, onLanguageChange }) => {
             </label>
             <select 
                 id="freestyle-language-select" 
-                value={selectedLanguage} 
+                value={currentLangKey || ''} 
                 onChange={handleChange} 
                 className="language-select-dropdown" /* Use unified class */
                 aria-label="Select language for freestyle mode"
