68bb13fdd99a2c7fdc46dec0ca9f3e0331d4c391
cosylanguages <cosylanguages@gmail.com>
2025-07-09 15:17:43 +0000

    new update

---
+++ build/data/vocabulary/dictionary/br/a0.js
@@ -0,0 +1,192 @@
+export const a0_breton_vocabulary = {
+  "Gerioù Diazez (Basic Words)": [
+    {
+      "term": "Demat",
+      "latinisation": "Demat",
+      "pronunciation": "DE-mat",
+      "partOfSpeech": "etrebroadell (etrebroadell)",
+      "definition": "Salud jeneral implijet e-pad an deiz. (Salud jeneral implijet e-pad an deiz.) - General greeting used during the day.",
+      "example": "Demat deoc'h! (Demat deoc'h!) - Hello to you (formal/plural)!",
+      "level": "a0",
+      "notes": "Salud a zo muioc'h anformeL. (Salud a zo muioc'h anformeL.)"
+    },
+    {
+      "term": "Kenavo",
+      "latinisation": "Kenavo",
+      "pronunciation": "ke-NA-vo",
+      "partOfSpeech": "etrebroadell (etrebroadell)",
+      "definition": "Evit kimiadiñ. (Evit kimiadiñ.) - For taking leave.",
+      "example": "Kenavo, betek warc'hoazh! (Kenavo, betek warc'hoazh!) - Goodbye, until tomorrow!",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Ya",
+      "latinisation": "Ya",
+      "pronunciation": "YA",
+      "partOfSpeech": "adverb (adverb)",
+      "definition": "Respont pozitivel. (Respont pozitivel.) - Affirmative answer.",
+      "example": "Ya, mar plij. (Ya, mar plij.) - Yes, please.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Nann",
+      "latinisation": "Nann",
+      "pronunciation": "NANN",
+      "partOfSpeech": "adverb (adverb)",
+      "definition": "Respont negativel. (Respont negativel.) - Negative answer.",
+      "example": "Nann, trugarez. (Nann, trugarez.) - No, thank you.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Trugarez",
+      "latinisation": "Trugarez",
+      "pronunciation": "troo-GA-rez",
+      "partOfSpeech": "anv-kadarn/etrebroadell (anv-kadarn/etrebroadell)",
+      "definition": "Ger evit diskouez anaoudegezh-vat. (Ger evit diskouez anaoudegezh-vat.) - Word to show gratitude.",
+      "example": "Trugarez vras! (Trugarez vras!) - Thank you very much!",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Mann ebet",
+      "latinisation": "Mann ebet",
+      "pronunciation": "mann e-BET",
+      "partOfSpeech": "troienn (troienn)",
+      "definition": "Respont seven da 'trugarez'. (Respont seven da 'trugarez'.) - Polite response to 'thank you'.",
+      "example": "Trugarez! - Mann ebet. (Trugarez! - Mann ebet.) - Thank you! - You're welcome.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Mar plij",
+      "latinisation": "Mar plij",
+      "pronunciation": "mar PLEEZH",
+      "partOfSpeech": "troienn (troienn)",
+      "definition": "Evit goulenn un dra bennak gant sevenidigezh. (Evit goulenn un dra bennak gant sevenidigezh.) - To ask for something politely.",
+      "example": "Un te, mar plij. (Un te, mar plij.) - A tea, please.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Mat eo",
+      "latinisation": "Mat eo",
+      "pronunciation": "mat EH-oh",
+      "partOfSpeech": "troienn (troienn)",
+      "definition": "Chom / OK / Mat pell zo. (Chom / OK / Mat pell zo.) - Alright / OK / It's good.",
+      "example": "Mat eo, mont a ran. (Mat eo, mont a ran.) - Alright, I'm going.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Unan",
+      "latinisation": "Unan",
+      "pronunciation": "OO-nan",
+      "partOfSpeech": "niver/raganv (niver/raganv)",
+      "definition": "An niver 1. (An niver 1.) - The number 1.",
+      "example": "Unan hepken. (Unan hepken.) - Only one.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Te",
+      "latinisation": "Te",
+      "pronunciation": "TE",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Evaj tomm graet gant delioù te. (Evaj tomm graet gant delioù te.) - Hot drink made from tea leaves.",
+      "example": "Un banne te? (Un banne te?) - A cup of tea?",
+      "level": "a0",
+      "notes": "Peurliesañ 'un te'. (Peurliesañ 'un te'.)"
+    },
+    {
+      "term": "Kafe",
+      "latinisation": "Kafe",
+      "pronunciation": "KA-fe",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Evaj graet gant greun kafe rostet. (Evaj graet gant greun kafe rostet.) - Drink made from roasted coffee beans.",
+      "example": "Evit kafe e c'hortozan. (Evit kafe e c'hortozan.) - I'm waiting for coffee.",
+      "level": "a0",
+      "notes": "Peurliesañ 'ur c'hafe' (gant kemmadur). (Peurliesañ 'ur c'hafe' (gant kemmadur).)"
+    },
+    {
+      "term": "Dour",
+      "latinisation": "Dour",
+      "pronunciation": "DOOR",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Liñvenn diliv, dic'hwezh, ret evit ar vuhez. (Liñvenn diliv, dic'hwezh, ret evit ar vuhez.) - Colorless, odorless liquid, necessary for life.",
+      "example": "Dour, mar plij. (Dour, mar plij.) - Water, please.",
+      "level": "a0",
+      "notes": "Peurliesañ 'un dour'. (Peurliesañ 'un dour'.)"
+    },
+    {
+      "term": "Chug",
+      "latinisation": "Chug",
+      "pronunciation": "SHOOK",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Liñvenn tennet eus frouezh pe legumaj. (Liñvenn tennet eus frouezh pe legumaj.) - Liquid obtained from fruit or vegetables.",
+      "example": "Chug orañjez. (Chug orañjez.) - Orange juice.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Bier",
+      "latinisation": "Bier",
+      "pronunciation": "BEE-er",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Evaj alkolek graet diwar heiz goeñvet. (Evaj alkolek graet diwar heiz goeñvet.) - Alcoholic drink made from fermented barley.",
+      "example": "Ur bier fresk. (Ur bier fresk.) - A cold beer.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Gwin",
+      "latinisation": "Gwin",
+      "pronunciation": "GWEEN",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "m",
+      "definition": "Evaj alkolek graet diwar rezin goeñvet. (Evaj alkolek graet diwar rezin goeñvet.) - Alcoholic drink made from fermented grapes.",
+      "example": "Gwin ruz pe gwin gwenn? (Gwin ruz pe gwin gwenn?) - Red wine or white wine?",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Arc'hant dizolo",
+      "latinisation": "Arc'hant dizolo",
+      "pronunciation": "ar-CHANT dee-ZO-lo",
+      "partOfSpeech": "troienn anv-kadarn (troienn anv-kadarn)",
+      "gender": "m",
+      "definition": "Pezhioù moneiz pe bilhedoù. (Pezhioù moneiz pe bilhedoù.) - Coins or banknotes.",
+      "example": "Nemet arc'hant dizolo a gemeran. (Nemet arc'hant dizolo a gemeran.) - I only take cash.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Kartenn",
+      "latinisation": "Kartenn",
+      "pronunciation": "KAR-tenn",
+      "partOfSpeech": "anv-kadarn (anv-kadarn)",
+      "gender": "f",
+      "definition": "Moyen paeañ plastik (kred pe debit). (Moyen paeañ plastik (kred pe debit). ) - Plastic means of payment (credit or debit).",
+      "example": "Paeañ gant kartenn. (Paeañ gant kartenn.) - To pay by card.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Digarezit",
+      "latinisation": "Digarezit",
+      "pronunciation": "dee-ga-RE-zeet",
+      "partOfSpeech": "verb/etrebroadell (verb/etrebroadell)",
+      "definition": "Evit en em zigareziñ pe goulenn un dra bennak (furmel pe lies). (Evit en em zigareziñ pe goulenn un dra bennak (furmel pe lies).) - To apologize or ask for something (formal or plural).",
+      "example": "Digarezit, pelec'h emañ an ti-post? (Digarezit, pelec'h emañ an ti-post?) - Excuse me, where is the post office?",
+      "level": "a0",
+      "notes": "AnformeL unan: Digarez. (AnformeL unan: Digarez.)"
+    }
+  ]
+};
---
+++ build/data/vocabulary/dictionary/ru/a0.js
@@ -0,0 +1,182 @@
+export const a0_russian_vocabulary = {
+  "Основные слова (Basic Words)": [
+    {
+      "term": "Здравствуйте",
+      "latinisation": "Zdravstvuyte",
+      "pronunciation": "ZDRAST-vooy-tye",
+      "partOfSpeech": "междометие (mezhdometiye)",
+      "definition": "Формальное приветствие. (Formal'noye privetstviye.) - Formal greeting.",
+      "example": "Здравствуйте, Иван Петрович! - Hello, Ivan Petrovich!",
+      "level": "a0",
+      "notes": "Привет (Privet) - неформальное приветствие. (neformal'noye privetstviye.)"
+    },
+    {
+      "term": "До свидания",
+      "latinisation": "Do svidaniya",
+      "pronunciation": "da svee-DA-nee-ye",
+      "partOfSpeech": "фраза (fraza)",
+      "definition": "Формальное прощание. (Formal'noye proshchaniye.) - Formal farewell.",
+      "example": "До свидания, увидимся завтра. - Goodbye, see you tomorrow.",
+      "level": "a0",
+      "notes": "Пока (Poka) - неформальное прощание. (neformal'noye proshchaniye.)"
+    },
+    {
+      "term": "Да",
+      "latinisation": "Da",
+      "pronunciation": "DA",
+      "partOfSpeech": "частица/наречие (chastitsa/narechiye)",
+      "definition": "Утвердительный ответ. (Utverditel'nyy otvet.) - Affirmative answer.",
+      "example": "Да, конечно. - Yes, of course.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Нет",
+      "latinisation": "Net",
+      "pronunciation": "NYET",
+      "partOfSpeech": "частица/наречие (chastitsa/narechiye)",
+      "definition": "Отрицательный ответ. (Otritsatel'nyy otvet.) - Negative answer.",
+      "example": "Нет, спасибо. - No, thank you.",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Спасибо",
+      "latinisation": "Spasibo",
+      "pronunciation": "spa-SEE-ba",
+      "partOfSpeech": "междометие/существительное (mezhdometiye/sushchestvitel'noye)",
+      "definition": "Выражение благодарности. (Vyrazheniye blagodarnosti.) - Expression of gratitude.",
+      "example": "Большое спасибо! - Thank you very much!",
+      "level": "a0",
+      "notes": ""
+    },
+    {
+      "term": "Пожалуйста",
+      "latinisation": "Pozhaluysta",
+      "pronunciation": "pa-ZHA-looy-sta",
+      "partOfSpeech": "частица/междометие (chastitsa/mezhdometiye)",
+      "definition": "Ответ на 'спасибо' / Вежливая просьба. (Otvet na 'spasibo' / Vezhlivaya pros'ba.) - Response to 'thank you' / Polite request.",
+      "example": "Дайте, пожалуйста, книгу. - Give (me) the book, please. / Спасибо! - Пожалуйста. - Thank you! - You're welcome.",
+      "level": "a0",
+      "notes": "Означает и 'please', и 'you're welcome'. (Oznachayet i 'please', i 'you're welcome'.)"
+    },
+    {
+      "term": "Хорошо",
+      "latinisation": "Khorosho",
+      "pronunciation": "kha-ra-SHO",
+      "partOfSpeech": "наречие/междометие (narechiye/mezhdometiye)",
+      "definition": "Одобрение / Согласие. (Odobreniye / Soglasiye.) - Approval / Agreement.",
+      "example": "Хорошо, я сделаю это. - Alright, I will do it.",
+      "level": "a0",
+      "notes": "Ладно (Ladno) также означает ОК/согласен. (Ladno takzhe oznachayet OK/soglasen.)"
+    },
+    {
+      "term": "Один",
+      "latinisation": "Odin",
+      "pronunciation": "a-DEEN",
+      "partOfSpeech": "числительное/местоимение (chislitel'noye/mestoimeniye)",
+      "definition": "Число 1. (Chislo 1.) - The number 1.",
+      "example": "Один билет, пожалуйста. - One ticket, please.",
+      "level": "a0",
+      "notes": "Изменяется по родам: одна (ж.р.), одно (ср.р.). (Izmenyayetsya po rodam: odna (zh.r.), odno (sr.r.).)"
+    },
+    {
+      "term": "Чай",
+      "latinisation": "Chay",
+      "pronunciation": "CHAI",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "m",
+      "definition": "Напиток из заваренных чайных листьев. (Napitok iz zavarennykh chaynykh list'yev.) - Drink made from brewed tea leaves.",
+      "example": "Я люблю чёрный чай. - I love black tea.",
+      "level": "a0",
+      "notes": "(мужской род - muzhskoy rod)"
+    },
+    {
+      "term": "Кофе",
+      "latinisation": "Kofe",
+      "pronunciation": "KO-fye",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "m",
+      "definition": "Напиток из обжаренных кофейных зёрен. (Napitok iz obzharennykh kofeynykh zoren.) - Drink made from roasted coffee beans.",
+      "example": "Чашка кофе. - A cup of coffee.",
+      "level": "a0",
+      "notes": "Несклоняемое. (Neskonyayemoye.) (мужской род - muzhskoy rod)"
+    },
+    {
+      "term": "Вода",
+      "latinisation": "Voda",
+      "pronunciation": "va-DA",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "f",
+      "definition": "Прозрачная бесцветная жидкость. (Prozrachnaya bestsvetnaya zhidkost'.) - Transparent colorless liquid.",
+      "example": "Стакан воды. - A glass of water.",
+      "level": "a0",
+      "notes": "(женский род - zhenskiy rod)"
+    },
+    {
+      "term": "Сок",
+      "latinisation": "Sok",
+      "pronunciation": "SOK",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "m",
+      "definition": "Жидкий напиток из фруктов или овощей. (Zhidkiy napitok iz fruktov ili ovoshchey.) - Liquid drink from fruits or vegetables.",
+      "example": "Апельсиновый сок. - Orange juice.",
+      "level": "a0",
+      "notes": "(мужской род - muzhskoy rod)"
+    },
+    {
+      "term": "Пиво",
+      "latinisation": "Pivo",
+      "pronunciation": "PEE-va",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "n",
+      "definition": "Алкогольный напиток из солода и хмеля. (Alkohol'nyy napitok iz soloda i khmelya.) - Alcoholic drink from malt and hops.",
+      "example": "Бутылка пива. - A bottle of beer.",
+      "level": "a0",
+      "notes": "(средний род - sredniy rod)"
+    },
+    {
+      "term": "Вино",
+      "latinisation": "Vino",
+      "pronunciation": "vee-NO",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "n",
+      "definition": "Алкогольный напиток из винограда. (Alkohol'nyy napitok iz vinograda.) - Alcoholic drink from grapes.",
+      "example": "Красное вино. - Red wine.",
+      "level": "a0",
+      "notes": "(средний род - sredniy rod)"
+    },
+    {
+      "term": "Наличные",
+      "latinisation": "Nalichnyye",
+      "pronunciation": "na-LEECH-ni-ye",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "plural": true,
+      "definition": "Деньги в виде монет или банкнот. (Den'gi v vide monet ili banknot.) - Money in the form of coins or banknotes.",
+      "example": "Платить наличными. - To pay in cash.",
+      "level": "a0",
+      "notes": "Грамматически множественное число. (Grammaticheski mnozhestvennoye chislo.)"
+    },
+    {
+      "term": "Карта",
+      "latinisation": "Karta",
+      "pronunciation": "KAR-ta",
+      "partOfSpeech": "существительное (sushchestvitel'noye)",
+      "gender": "f",
+      "definition": "Банковская пластиковая карта. (Bankovskaya plastikovaya karta.) - Bank plastic card.",
+      "example": "Оплатить картой. - To pay by card.",
+      "level": "a0",
+      "notes": "Также карточка (kartochka). (zhenskiy rod - zhenskiy rod)"
+    },
+    {
+      "term": "Извините",
+      "latinisation": "Izvinite",
+      "pronunciation": "eez-vee-NEE-tye",
+      "partOfSpeech": "глагол/междометие (glagol/mezhdometiye)",
+      "definition": "Выражение сожаления / Просьба о прощении (формально или мн.ч.). (Vyrazheniye sozhaleniya / Pros'ba o proshchenii (formal'no ili mn.ch.).) - Expression of regret / Request for forgiveness (formal or plural).",
+      "example": "Извините, я опоздал. - Sorry, I am late.",
+      "level": "a0",
+      "notes": "Неформально ед.ч.: Извини (Izvini). Простите (Prostite) также распространено и схоже по формальности. (Neformal'no yed.ch.: Izvini (Izvini). Prostite (Prostite) takzhe rasprostraneno i skhozhe po formal'nosti.)"
+    }
+  ]
+};
