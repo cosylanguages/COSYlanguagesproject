836570acfbfb47b8ad35c02e9757aa302fe79fa4
damirmoskov <damirmoskov@users.noreply.github.com>
2025-07-18 09:29:15 +0000

    deploy: c9ed99765b1ea2cf3499554da78d28530f2015c8

---
+++ static/js/sw.ff931169.js
@@ -0,0 +1,2 @@
+(()=>{const e=["/","/index.html","/manifest.json","/favicon.ico","/logo192.png","/logo512.png"];self.addEventListener("install",n=>{n.waitUntil(caches.open("cosy-languages-cache-v1").then(n=>(console.log("Opened cache"),n.addAll(e))))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(n=>n||fetch(e.request)))})})();
+//# sourceMappingURL=sw.ff931169.js.map
